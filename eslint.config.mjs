d049c8629a4181fd9c385877106f9c870a220752
Austin Raey <slowpoke@slowp.ke>
2024-12-09 12:22:03 -0500

    📦 package update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,11 +9,8 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import ts from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
+	baseDirectory: path.dirname(fileURLToPath(import.meta.url)),
 });
 
 export default ts.config(
