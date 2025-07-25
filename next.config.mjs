2f6b8088ec41ba10c736415d22893a0d5a39efec
Austin Raey <slowpoke@slowp.ke>
2025-07-25 14:05:57 -0400

    🔧 adjustments to config to allow turbopack builds to work

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,6 @@
 const nextConfig = {
 	// assetPrefix: "/v1", // Consider adding this if running multiple instances of Next.js on the same host.
 
-	eslint: {
-		/**
-		 * When using Docker with Next.js, you probably do not want to bundle
-		 * eslint and eslint plugins into the image to save on size and reduce the
-		 * attack surface.
-		 *
-		 * With image layering, you could remove this configuration and simply copy the
-		 * build artifacts to the final image. If your workflow requires you to copy `node_modules` however,
-		 * this is something to consider.
-		 */
-		ignoreDuringBuilds: true,
-	},
-
 	experimental: {
 		inlineCss: true,
 		// ppr: true, // Requires next@canary
